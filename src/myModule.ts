f4766232fd4172e37e5ffe52d5755ac11ea0a9d4
Leonie113 <57359744+Leonie113@users.noreply.github.com>
2020-04-17 11:51:36 +0200

    Object is possibly null

--- src/myModule.ts
+++ src/myModule.ts
@@ -37,13 +37,8 @@ cardsData = getCardsData();
 
 // Create all cards
 function createCards() {
-
-    if (cardsData != null)
-    {
-      cardsData.forEach((data: any, index: any) => createCard(data, index));
-    }
-    
-  }
+  cardsData.forEach((data: any, index: any) => createCard(data, index));
+}
 
 // Create a single card in DOM
 function createCard(data: any, index: any) {
@@ -73,164 +68,102 @@ function createCard(data: any, index: any) {
   
     // Add to DOM cards
     cardsEl.push(card);
-    
-    if(cardsContainer) 
-      cardsContainer.appendChild(card);
-  
+    cardsContainer!.appendChild(card);
     updateCurrentText();
-
-
-    
-  }
+}
   
 
 
   // Show number of cards
-  function updateCurrentText() {
-    if(currentEl) {
-    currentEl.innerText = `${currentActiveCard + 1}/${cardsEl.length}`;
-  }}
+function updateCurrentText() {
+  currentEl!.innerText = `${currentActiveCard + 1}/${cardsEl.length}`;
+}
   
   // Get cards from local storage
-  function getCardsData(): any {
+function getCardsData(): any {
 
-    var tmpCards : any | null ;
-    tmpCards = localStorage.getItem('cards');
+  var tmpCards : any | null ;
+  tmpCards = localStorage.getItem('cards');
 
-    var cards = null;
-    if (typeof tmpCards === 'string') { 
-       var cards = JSON.parse(tmpCards); }
-       
-    return cards === null ? [] : cards;
-    
-  }
+  var cards = null;
+  if (typeof tmpCards === 'string') { 
+    var cards = JSON.parse(tmpCards); }
+  return cards === null ? [] : cards;
+}
  
 
   
   // Add card to local storage
-  function setCardsData(cards: any):any {
-
-        localStorage.setItem('cards', JSON.stringify(cards));
-        window.location.reload();
-      
-  }
-
-  
-  createCards();
+function setCardsData(cards: any):any {
+  localStorage.setItem('cards', JSON.stringify(cards));
+  window.location.reload();
+}
+createCards();
   
 // ___Event listeners___
 
     // Next button
 
-
-  if(nextBtn) {
-    nextBtn.addEventListener('click', () => 
-    {
-      cardsEl[currentActiveCard].className = 'card left';
-
-        currentActiveCard = currentActiveCard + 1;
-
-        if (currentActiveCard > cardsEl.length - 1) {
-            currentActiveCard = cardsEl.length - 1;
-        }
-    
-        cardsEl[currentActiveCard].className = 'card active';
-        
-        updateCurrentText();
-
-    }
-  )}
-
-
-
+nextBtn!.addEventListener('click', () => {
+  cardsEl[currentActiveCard].className = 'card left';
+  currentActiveCard = currentActiveCard + 1;
+  if (currentActiveCard > cardsEl.length - 1) {
+    currentActiveCard = cardsEl.length - 1; }
+  cardsEl[currentActiveCard].className = 'card active';
+  updateCurrentText();
+});
 
   // Prev button
-
-  if(prevBtn) {
-  prevBtn.addEventListener('click', () => {
-    cardsEl[currentActiveCard].className = 'card right';
-  
-    currentActiveCard = currentActiveCard - 1;
-  
-    if (currentActiveCard < 0) {
+prevBtn!.addEventListener('click', () => {
+  cardsEl[currentActiveCard].className = 'card right';
+  currentActiveCard = currentActiveCard - 1;
+  if (currentActiveCard < 0) {
       currentActiveCard = 0;
-    }
-  
-    cardsEl[currentActiveCard].className = 'card active';
-  
-    updateCurrentText();
+  }
+  cardsEl[currentActiveCard].className = 'card active';
+  updateCurrentText();
   });
-}
+
 
 
   // Show add container
-
-  if(showBtn) {
-
-    showBtn.addEventListener('click', () => {
-      if(addContainer) 
-        addContainer.classList.add('show');
-        
-    });
-  }
+showBtn!.addEventListener('click', () => {
+  if(addContainer) 
+    addContainer.classList.add('show');   
+});
+  
 
   // Hide add container
-  if(hideBtn) {
-  hideBtn.addEventListener('click', () => {
-      if(addContainer)
-        addContainer.classList.remove('show');
+hideBtn!.addEventListener('click', () => {
+  if(addContainer)
+    addContainer.classList.remove('show');
     });
-  } 
 
-  
   // Add new card
-
-    if(addCardBtn) {
-
-      addCardBtn.addEventListener('click', () => {
-
-        const question : string = (<HTMLInputElement>questionEl).value;
-        const answer : string = (<HTMLInputElement>answerEl).value;
-
-
-        if (question && answer) {
-          if (question.trim() && answer.trim()) {
-            var newCard = new Array;
-            newCard[0] = question;
-            newCard[1] = answer;
+addCardBtn!.addEventListener('click', () => {
+  const question : string = (<HTMLInputElement>questionEl).value;
+  const answer : string = (<HTMLInputElement>answerEl).value;
+    if (question.trim() && answer.trim()) {
+      var newCard = new Array;
+      newCard[0] = question;
+      newCard[1] = answer;
             
-           
-            //createCard(newCard,0);
-            createCard(newCard,0);
-
-            (<HTMLInputElement>questionEl).value = '';
-            (<HTMLInputElement>answerEl).value = '';
-
-          if (addContainer)    
-            addContainer.classList.remove('show');
-
-            //cardsData.concat(question, answer);
-
-            cardsData.push(newCard);
-            setCardsData(cardsData);
+  //createCard(newCard,0);
+      createCard(newCard,0);
+      (<HTMLInputElement>questionEl).value = '';
+      (<HTMLInputElement>answerEl).value = '';  
+        addContainer!.classList.remove('show');
+        cardsData.push(newCard);
+        setCardsData(cardsData);
             
-        }
-      }
-      });
-    }   
-    
-  
+    }
+}); 
 
   // Clear cards buttons
-
-  if(clearBtn){
-    clearBtn.addEventListener('click', () => {
-        if(localStorage){
-            localStorage.clear();}
-        if(cardsContainer){
-            cardsContainer.innerHTML = '';}
-        window.location.reload();
-        });
-    }
+clearBtn!.addEventListener('click', () => {
+  localStorage.clear();
+  cardsContainer!.innerHTML = '';
+  window.location.reload(); 
+});
  
 
